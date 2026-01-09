0f39755fd94044244f83ecff4078cb9d13d5b13d
Jonathan Oliveira <jonathanoliv7@gmail.com>
2026-01-09 01:59:38 -0300

    finalizado

--- src/Aplication/App.java
+++ src/Aplication/App.java
@@ -10,18 +10,24 @@ public class App {
 		int n = sc.nextInt();
 		int[] lista1 = new int[n];
 		int[] lista2 = new int[n];
-		
+		int[] somaTotal = new int[n];
+
 		System.out.println("Digite os numeros do vetor A");
-		for(int i =0; i < n; i++) {
-			lista1[i] = sc.nextInt();	
+		for (int i = 0; i < n; i++) {
+			lista1[i] = sc.nextInt();
 		}
 		System.out.println("Digite os numeros do vetor B");
-		for(int i =0; i < n; i++) {
-			lista1[i] = sc.nextInt();	
+		for (int i = 0; i < n; i++) {
+			lista2[i] = sc.nextInt();
 		}
-		
-		
-	sc.close();	
+
+		for (int i = 0; i < n; i++) {
+			somaTotal[i] = lista1[i] + lista2[i];
+		}
+		System.out.println("VETOR RESULTANTE:");
+		for (int num : somaTotal)
+			System.out.println(num);
+		sc.close();
 	}
 
 }
