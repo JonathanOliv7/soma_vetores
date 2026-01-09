cfb8bfc2cdcc58a955aec1b1f7545adf778124c3
Jonathan Oliveira <jonathanoliv7@gmail.com>
2026-01-09 01:52:11 -0300

    inicio

---
+++ src/Aplication/App.java
@@ -0,0 +1,27 @@
+package Aplication;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Quantos valores vai ter cada vetor?");
+		int n = sc.nextInt();
+		int[] lista1 = new int[n];
+		int[] lista2 = new int[n];
+		
+		System.out.println("Digite os numeros do vetor A");
+		for(int i =0; i < n; i++) {
+			lista1[i] = sc.nextInt();	
+		}
+		System.out.println("Digite os numeros do vetor B");
+		for(int i =0; i < n; i++) {
+			lista1[i] = sc.nextInt();	
+		}
+		
+		
+	sc.close();	
+	}
+
+}
